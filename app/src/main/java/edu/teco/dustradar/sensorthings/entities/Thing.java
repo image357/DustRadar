1689276c6adf04b350e21734e106aaaa515fcb6f
Marcel Köpke <marcel.koepke@gmail.com>
2018-09-10 18:19:28 +0200

    Thing implementation

--- app/src/main/java/edu/teco/dustradar/sensorthings/entities/Thing.java
+++ app/src/main/java/edu/teco/dustradar/sensorthings/entities/Thing.java
@@ -1,17 +1,52 @@
 package edu.teco.dustradar.sensorthings.entities;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
 public class Thing {
 
     // private members
 
     private String name = null;
     private String description = null;
-    private String properties = null;
+    private Object properties = null;
 
 
     // constructors
 
     public  Thing() {
+    }
+
+
+    // public methods
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String arg) {
+        name = arg;
+    }
+
+
+    public String getDescription () {
+        return description;
+    }
+
+    public void setDescription(String arg) {
+        description = arg;
+    }
+
+
+    public Object getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Object arg) {
+        properties = arg;
     }
 
 }
